19cb5f9af27cd391066263e40b4c932406d777f4
Everfall23 <barry.haroun@edu.futuregames.nu>
2023-09-12 12:10:32 +0200

    Created Soul and soul generator

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,41 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
-public class PlayerMovements : MonoBehaviour
+public class PenguinMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 100f;
-    public float jumpForce = 300f;
-    public  Rigidbody rigidBody;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Cursor.lockState = CursorLockMode.Locked;
-    }
-
-
-
-    // Update is called once per frame
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float rotationSpeed = 20f;
+    [SerializeField] private float jumpForce = 300f;
+    [SerializeField] private Rigidbody penguinBody;
     void Update()
     {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
-        float jumpForce = 300f;
-
-        Vector3 moveDirection = new Vector3 (horizontalMove, 0, verticalMove);
-        moveDirection.Normalize();
-        float magnitude = moveDirection.magnitude;
-        magnitude = Mathf.Clamp01(magnitude);
-        transform.Translate(moveDirection * magnitude * speed *Time.deltaTime, Space.World);
-
-        if(moveDirection != Vector3.zero)
+        if (Input.GetAxis("Horizontal") != 0)
         {
-            Quaternion toRotate = Quaternion.LookRotation(moveDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
+            Vector3 direction = transform.up * Input.GetAxis("Horizontal");
+            transform.Rotate(direction * rotationSpeed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.Space)) rigidBody.AddForce(Vector3.up * jumpForce);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Vector3 direction = transform.forward * Input.GetAxis("Vertical");
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !IsTouchingFloor())
+        {
+            penguinBody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    private bool IsTouchingFloor()
+    {
+        RaycastHit hit;
+        bool result = Physics.SphereCast(transform.position, 0.15f, -transform.up, out hit, 1);
+        return result;
     }
 }
